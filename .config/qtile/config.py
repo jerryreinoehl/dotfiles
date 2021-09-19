ec707e9834ff30acdd1fb0ecf32deedeeccc5b5b
Jerry Reinoehl <jerryreinoehl@gmail.com>
2021-09-19 15:17:04 -0500

    Use date time group format in qtile status bar

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -141,7 +141,7 @@ screens = [
                 widget.TextBox("default config", name="default"),
                 widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+                widget.Clock(format='%d%H%MS%^b%y'),
                 widget.QuickExit(),
             ],
             24,
