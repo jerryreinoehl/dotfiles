c7a210b613087c7a9b75ccc1cb18a6fa2f17d86c
Jerry Reinoehl <jerryreinoehl@gmail.com>
2021-10-16 22:18:14 -0500

    Add zsh prompt script

---
+++ .config/zsh/source/prompt.zsh
@@ -0,0 +1,76 @@
+precmd() {
+    # pipestatus will be overwritten after the first command
+    __pipestatus="$pipestatus"
+    __prompt
+}
+
+# ----------------------------------------------------------------------------
+__prompt() {
+    local ptr branch error bgjobs REPLY
+
+    __prompt_pointer; ptr="$REPLY"
+    __prompt_error; error="$REPLY"
+    __prompt_bgjobs; bgjobs="$REPLY"
+    __prompt_branch; branch="$REPLY"
+
+    PS1="%B%F{green}%n@%m%f %F{blue}%1~%f%b $ptr "
+
+    RPS1=""
+    [[ ! -z "$error" ]] && RPS1+="$error"
+    [[ ! -z "$bgjobs" ]] && RPS1+=" $bgjobs"
+    [[ ! -z "$branch" ]] && RPS1+=" $branch"
+}
+
+# ----------------------------------------------------------------------------
+__prompt_pointer() {
+    [[ "$__pipestatus" =~ ^0( 0)*$ ]] \
+        && REPLY="%B%(!.#.>)%b" \
+        || REPLY="%B%F{red}%(!.#.>)%f%b"
+}
+
+# ----------------------------------------------------------------------------
+__prompt_error() {
+    [[ "$__pipestatus" =~ ^0( 0)*$ ]] \
+        && REPLY="" \
+        || REPLY="%B%F{red}[$__pipestatus]%f%b"
+}
+
+# ----------------------------------------------------------------------------
+__prompt_bgjobs() {
+    local -i num_jobs=${#jobtexts[@]}
+    (( num_jobs > 0 )) \
+        && REPLY="%B%F{white}*$num_jobs%f%b" \
+        || REPLY=""
+}
+
+# ----------------------------------------------------------------------------
+__prompt_branch() {
+    local head branch
+
+    __prompt_git_head "$PWD"; head="$REPLY"
+
+    [[ -z "$head" ]] && REPLY="" && return
+    __prompt_git_branch "$head"; branch="$REPLY"
+
+    [[ -z "$branch" ]] && REPLY="" && return
+    REPLY=$'%B%F{magenta}\u2387 '$branch'%f%b'
+}
+
+# ----------------------------------------------------------------------------
+__prompt_git_head() {
+    local dir="$1"
+    REPLY=""
+
+    while [[ ! -z "$dir" ]]; do
+        [[ -r "$dir/.git/HEAD" ]] && REPLY="$dir/.git/HEAD" && return
+        dir="${dir%/*}"
+    done
+}
+
+# ----------------------------------------------------------------------------
+__prompt_git_branch() {
+    local head="$1" ref
+
+    read ref < "$head"
+    [[ "$ref" =~ ^ref: ]] && REPLY="${ref##*/}" || REPLY="${ref:0:6}"
+}
