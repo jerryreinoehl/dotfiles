f4b29b80ba81f3af26ec4504eb7ea8081b055043
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-02-28 22:26:16 -0600

    Configure latex lsp

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -1,1 +1,32 @@
-require("lsp").setup("texlab")
+require("lsp").setup {
+  cmd = {"texlab"},
+  single_file_support = true,
+  settings = {
+    texlab = {
+      rootDirectory = nil,
+      build = {
+        executable = "latexmk",
+        args = {"-pdf", "-interaction=nonstopmode", "-synctex=1", "%f"},
+        onSave = true,
+        forwardSearchAfter = false,
+      },
+      auxDirectory = ".",
+      forwardSearch = {
+        executable = nil,
+        args = {},
+      },
+      chktex = {
+        onOpenAndSave = true,
+        onEdit = true,
+      },
+      diagnosticDelay = 300,
+      latexFormatter = "latexindent",
+      latexindent = {
+        ["local"] = nil,
+        modifyLineBreaks = false,
+      },
+      bibtexFormatter = "texlab",
+      formatterLineLength = 80,
+    },
+  },
+}
