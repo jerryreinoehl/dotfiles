5220b6ef828e9e0ff9e16de18563d8a1efaf7ced
Jerry Reinoehl <jerryreinoehl@gmail.com>
2025-01-02 14:41:30 -0600

    nvim: Update python lsp config

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,1 +1,15 @@
-require("lsp").setup("pylsp")
+require("lsp").setup {
+  name = "pylsp",
+  cmd = {"pylsp"},
+  root_pat = {
+    ".git",
+    "pyproject.toml",
+    "setup.cfg",
+    "tox.ini",
+  }
+}
+
+vim.keymap.set("n", "<leader>f", function()
+  os.execute("black " .. vim.api.nvim_buf_get_name(0))
+  vim.cmd.edit()
+end)
