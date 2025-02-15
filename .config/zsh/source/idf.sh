c38098a9a3d174aee960c6f60470585845be5c33
Jerry Reinoehl <jerryreinoehl@gmail.com>
2025-02-14 20:49:51 -0600

    zsh: Add idf alias

---
+++ .config/zsh/source/idf.sh
@@ -0,0 +1,13 @@
+# =============================================================================
+# Creates the `idf` command if esp-idf is installed.
+#
+# `idf` is just an alias for `idf.py` but will also source the esp-idf
+# environment if not already done so (by checking if `IDF_PATH` has been set).
+# =============================================================================
+
+[[ ! -r /opt/esp-idf/export.sh ]] && return
+
+idf() {
+  [[ -z $IDF_PATH ]] && source /opt/esp-idf/export.sh
+  idf.py "$@"
+}
