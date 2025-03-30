53078c21e47abf069d0d6a1b871259caa7c40202
Jerry Reinoehl <jerryreinoehl@gmail.com>
2025-03-30 12:10:26 -0500

    nvim: Run go fmt on save

--- .config/nvim/ftplugin/go.lua
+++ .config/nvim/ftplugin/go.lua
@@ -2,3 +2,9 @@ require("lsp").setup("gopls")
 
 vim.opt.expandtab = false
 vim.opt.shiftwidth = 4
+
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  callback = function()
+    vim.lsp.buf.format()
+  end
+})
